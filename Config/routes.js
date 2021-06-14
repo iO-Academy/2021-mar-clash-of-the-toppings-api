14a94eeb2e93670e3017306226e789410fe45f72
Maria Gusova <masha.gu@gmail.com>
2021-06-14 11:53:12 +0100

    create routes

--- Config/routes.js
+++ Config/routes.js
@@ -1,6 +1,7 @@
-
 const routes = (app) => {
+  app.get("/", ToppingsController.getAllToppings);
+  app.put("/", ToppingsController.updateTopping);
+  app.get("/topten", ToppingsController.getTopTen);
+};
 
-}
-
-module.exports = routes
+module.exports = routes;
