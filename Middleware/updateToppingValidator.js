295e3e5654e9a0325c48e96e3f755a1606f3ca66
Maria Gusova <masha.gu@gmail.com>
2021-06-15 10:28:15 +0100

    add validation

---
+++ Middleware/updateToppingValidator.js
@@ -0,0 +1,21 @@
+const ResponsesService = require("../Services/ResponsesService");
+
+const updateToppingValidator = (req, res, next) => {
+  const wins = req.body.wins;
+  const battles = req.body.battles;
+
+  if (checkInt(battles) && checkInt(wins) && battles >= wins) {
+    return next();
+  }
+  let jsonRes = ResponsesService.unsuccessful();
+  return res.json(jsonRes);
+};
+
+const checkInt = (value) => {
+  if (typeof value === "number" && String(value).match(/^[0-9]*$/gm)) {
+    return true;
+  }
+  return false;
+};
+
+module.exports.updateToppingValidator = updateToppingValidator;
