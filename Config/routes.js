2e654509773d36d73c129809ba0b8c922f3b46a7
Zedonk1995 <paulRenglish@yahoo.co.uk>
2021-06-17 11:58:52 +0100

    added all the standard routes tand gave the required 405 reponses to the make api more restful

--- Config/routes.js
+++ Config/routes.js
@@ -1,14 +1,20 @@
 const ToppingsController = require("../Controllers/ToppingsController");
+const InvalidMethodController = require("../Controllers/InvalidMethodController")
 const validator = require("../Middleware/updateToppingValidator");
 
 const routes = (app) => {
   app.get("/toppings", ToppingsController.getTwoToppings);
+  app.post("/toppings", InvalidMethodController.sendInvalidMethodResponse);
   app.put(
     "/toppings",
     validator.updateToppingValidator,
     ToppingsController.updateTopping
   );
+  app.delete("/toppings", InvalidMethodController.sendInvalidMethodResponse);
   app.get("/toppings/topten", ToppingsController.getTopTen);
+  app.post("/toppings/topten", InvalidMethodController.sendInvalidMethodResponse);
+  app.put("/toppings/topten", InvalidMethodController.sendInvalidMethodResponse);
+  app.delete("/toppings/topten", InvalidMethodController.sendInvalidMethodResponse);
 };
 
 module.exports = routes;
